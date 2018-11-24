65d6d347e84db1be72c5826ef2d6ab48253e40f2
kemushi54 <aychang4@gmail.com>
2018-11-24 06:11:59 +0000

    edit scrape.R for agent info

--- script/scrape.R
+++ script/scrape.R
@@ -6,54 +6,49 @@
 library(rvest)
 library(magrittr)
 library(data.table)
-library(stringr)
 library(RCurl)
-library(XML)
+library(tidyr)
+library(writexl)
+### url main page of maiz
+url.maiz <- "http://web.tari.gov.tw/techcd/%E8%94%AC%E8%8F%9C/%E6%9E%9C%E8%8F%9C%E9%A1%9E/%E7%8E%89%E7%B1%B3/%E8%9F%B2%E5%AE%B3/%E7%8E%89%E7%B1%B3-%E8%9F%B2%E5%AE%B3.htm"
 
-#-- 
-# for url
-url.1 <- "http://web.tari.gov.tw/techcd/%E8%94%AC%E8%8F%9C/%E6%9E%9C%E8%8F%9C%E9%A1%9E/%E7%8E%89%E7%B1%B3/%E8%9F%B2%E5%AE%B3/%E7%8E%89%E7%B1%B3-%E8%9F%B2%E5%AE%B3.htm"
+### get pest url list form main page
+pest.list.ori <- 
+  read_html(url.maiz) %>% 
+  html_nodes("tr td font a") %>% 
+  html_attr("href") %>% 
+  .[24:length(.)]
+  
+pest.list.url <- 
+  curlEscape(pest.list.ori)
 
-all.url <- 
-  read_html(url.1)
-
-# 擷取所有蟲害物種 [24:]
-content <- 
-  html_nodes(all.url, "tr td font a") %>% 
-  html_attr("href")
-
-curlEscape(content[24])
 ### 用文字轉url取得每個物種連結
 # pattern url.1: 玉米-蟲害 --> 玉米-蟲害名稱
 
 url.0 <- "http://web.tari.gov.tw/techcd/%E8%94%AC%E8%8F%9C/%E6%9E%9C%E8%8F%9C%E9%A1%9E/%E7%8E%89%E7%B1%B3/%E8%9F%B2%E5%AE%B3/"
 
-url.test <- sprintf("%s%s", 
-                    url.0, curlEscape(content[25]))
-### for loop or lapply 藥劑資訊
-
-
-#--
-# for content
-### 文字可能ok，還要想辦法表格化
-
-# # xpath for 
-# xpath <- '//*[@id="table3"]'
-# table 會落在1-4之間，也有可能沒有...
+pest.url <- sprintf("%s%s", 
+                    url.0, pest.list.url)
+### use lapply get 藥劑資訊
 pest.info <- 
-  read_html(url.test) %>% 
-  html_table(fill = TRUE)
-
-
-# pest.info <- 
-#   read_html(url.test) %>% 
-#   html_nodes(xpath = xpath) %>% 
-#   html_text %>% 
-#   str_replace_all("\\s", "_") %>% 
-#   str_split(pattern = "_") %>% 
-#   unlist
-
-# 
-# text.clean <- 
-#   pest.info[pest.info != ""]
-
+  lapply(1:length(pest.url), 
+         function(x)
+           read_html(pest.url[x]) %>% 
+           html_table(fill = TRUE) %>% 
+           lapply(function(i)
+             if(dim(i)[2] == 5){i}) %>% 
+           do.call(rbind, .) %>% 
+           setDT %>% 
+           .[-1] %>% 
+           .[, Pest := pest.list.ori[x]]
+  ) %>% 
+  do.call(rbind, .)
+  
+### cleaning table
+pest.info.clean <- 
+  pest.info %>% 
+  setnames(names(pest.info)[1:5], 
+           paste0(pest.info[1, 1:5])) %>% 
+  .[!(藥劑名稱 %like% "藥劑名稱|費雯綺")]
+write_xlsx(pest.info.clean,
+           "agent_info_maiz.xlsx")
