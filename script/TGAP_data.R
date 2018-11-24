3686af72cf7c492e20f62a2ee045f21602560525
kemushi54 <aychang4@gmail.com>
2018-11-24 21:50:23 +0000

    modified TGAP data scrape.R and pest image

--- script/TGAP_data.R
+++ script/TGAP_data.R
@@ -8,13 +8,17 @@ TGAP <-
   setDT %>% 
   .[, Agent := gsub("\\d|\\.|%|[a-zA-Z]|/", "", 使用防治資材)] %>% 
   .[, Agent := gsub("溶液|粒劑|可濕性|水分散性|乳劑|粉劑", "", Agent)]
-  ## 無法處理：粒劑  
+  ## 無法處理：粒劑  ( <- modified in excel)
 
 write_xlsx(TGAP, "Results/TGAP_data.xlsx")
 
 
 ### use pest name to get photo url (google search engine)
 library(rvest)
+
+# reload modidied TGAP_data
+TGAP <- 
+  read_xlsx("Results/TGAP_data.xlsx")
 
 search.pest.sci <- 
   TGAP$防治對象 %>% unique
@@ -23,20 +27,35 @@ search.pest.sci <-
 google.url <- sprintf("https://www.google.com.tw/search?q=%s&source=lnms&tbm=isch&sa=X&ved=0ahUKEwi4ibz_te3eAhXIXLwKHVe5AWIQ_AUIDigB&biw=1280&bih=610",
                       search.pest.sci)
 
+# get url of pest image search from google
 google.image.url <- 
   lapply(google.url, 
          function(x)
            read_html(x) %>% 
            html_nodes(xpath = '//img') %>% 
            html_attr("src") %>% 
-           .[1]) %>% 
-  do.call(rbind, .)
+           .[1:10])
 
-pest.google.image <- 
-  data.frame(pest.Sci = search.pest.sci,
-             image.url = google.image.url)
+# download image to folder
+lapply(search.pest.sci, 
+       function(x)
+         ifelse(dir.exists(sprintf("Results/%s", x)), 
+                FALSE, 
+                dir.create(sprintf("Results/%s", x))))
+lapply(1:length(google.image.url),
+       function(x)
+         lapply(1:length(google.image.url[[x]]),
+                function(i)
+                  download.file(google.image.url[[x]][i], 
+                                sprintf("Results/%s/IMG_%s.jpg",
+                                        search.pest.sci[x], i),
+                                mode = "wb"))) %>% 
+  invisible
 
-write_xlsx(pest.google.image,
-           "Results/TGAPpest_googleIMG.xlsx")
+# pest.google.image <- 
+#   data.frame(pest.Sci = search.pest.sci,
+#              image.url = google.image.url)
+# 
+# write_xlsx(pest.google.image,
+#            "Results/TGAPpest_googleIMG.xlsx")
 
-  
