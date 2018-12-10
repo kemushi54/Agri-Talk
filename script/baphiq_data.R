f4c49dd0af1b1eb1f564634051db01db6c1f5362
kemushi54 <aychang4@gmail.com>
2018-12-10 18:39:50 +0800

    modified baphiq_data.R

--- script/baphiq_data.R
+++ script/baphiq_data.R
@@ -4,38 +4,54 @@ library(magrittr)
 library(rvest)
 library(writexl)
 
-# Sys.setlocale("LC_ALL", "C")
-# Sys.getlocale(category = "LC_ALL")
+url.dat <-
+  read_xlsx("data/防檢局作物蟲害對應化學藥劑資料表.xlsx",
+            col_names = TRUE) %>% 
+  setDT %>% 
+  # remove non-link item
+  .[`防治藥劑` %like% "https"]
 
-# import url
-url.dat <- 
-  read_xlsx("data/蟲害與藥劑url.xlsx",
-            col_names = FALSE)
-
-
-test.url <- 
-  "https://pesticide.baphiq.gov.tw/web/Insecticides_MenuItem5_5_B.aspx?tp=1&id1=B030107&id2=A010106&sign=X"
 
 content <- 
-  lapply(1:length(url.dat$X__2), 
+  lapply(1:nrow(url.dat), 
          function(x)
-           sprintf("https://pesticide.baphiq.gov.tw/web/%s", url.dat$`X__2`[x]) %>% 
-           read_html() %>% 
-           html_nodes("table#GridView1") %>% 
-           html_table %>% 
-           as.data.table %>% 
-           .[, Pest := url.dat$X__1[x]]) %>% 
+           tryCatch({
+             read_html(url.dat$防治藥劑[x]) %>% 
+               html_nodes("table#GridView1") %>% 
+               html_table  %>%
+                as.data.table %>%
+               .[, Crop := url.dat$TGAP品項[x]] %>%
+               .[, Pest := url.dat$TGAP害蟲[x]]
+             # Sys.sleep(2)
+             },
+             error = function(msg){
+               message(paste(url.dat$TGAP品項[x], url.dat$TGAP害蟲[x], 
+                             sep = "_", collapse = "\r"))
+             })
+  ) %>%
   do.call(rbind, .)
-  
-#########
 
-tttt.url <- 
-  "https://pesticide.baphiq.gov.tw/web/Insecticides_MenuItem5_5.aspx?fbclid=IwAR0338cG-ggIad981OY7nE8p1quZ04a6qbCQ_uSifFqyCHEFG51LMHu-Rbs"
+write_xlsx(content, "Results/Baphiq_info.xlsx")
 
-cot <- 
-  read_html(tttt.url) %>% 
-  html_nodes(xpath = '//*[(@id="ctl00_ContentPlaceHolder1_updPnl")]') %>%
-  html_text
 
-"#ctl00_ContentPlaceHolder1_updPnl table"
-//*[@id="ctl00_ContentPlaceHolder1_updPnl"]/table[2]/tbody/tr[2]/td
+###############################
+# join with item name
+item <- 
+  fread("data/農藥名稱手冊.csv") %>% 
+  .[, list(`普通名稱` = Name, 
+           EnName)] %>% 
+  unique
+item.2 <- 
+  read_xlsx("data/農藥資料查詢.xlsx") %>% 
+  setDT %>% 
+  .[, list(`普通名稱` = `中文名稱`,
+           EnName = `英文名稱`)] %>% 
+  unique
+
+item.all <- 
+  rbind(item, item.2) %>% 
+  .[, EnName := "TT"] %>% 
+  unique
+
+final.dat <- 
+  item.all[content, on = "普通名稱"]
